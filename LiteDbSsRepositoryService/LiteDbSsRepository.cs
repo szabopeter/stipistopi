e623332bd18357d78f323c3d2a282eb4e8182587
Szabó Péter <1254135+szabopeter@users.noreply.github.com>
2020-05-24 06:57:06 +0000

    Added empty LiteDbSsRepositoryService project

---
+++ LiteDbSsRepositoryService/LiteDbSsRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using ServiceInterfaces;
+using ServiceInterfaces.Dto;
+
+namespace LiteDbSsRepositoryService
+{
+    public class LiteDbSsRepository : ISsRepository
+    {
+        public SsUserSecret Authenticated(SsUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<SsResource> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SsUser GetLockedBy(SsResource ssr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsLocked(SsResource res)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Lock(SsResource resource, SsUserSecret user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NewResource(SsResource ssResource)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NewUser(SsUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Release(SsResource resource, SsUserSecret user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
