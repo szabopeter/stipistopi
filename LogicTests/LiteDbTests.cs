388fb9e1c3d097695550c0675a64fe9cb3b5118d
Szabó Péter <1254135+szabopeter@users.noreply.github.com>
2020-05-29 18:43:55 +0200

    Remove temp files after tests

--- LogicTests/LiteDbTests.cs
+++ LogicTests/LiteDbTests.cs
@@ -1,14 +1,29 @@
+using System;
+using System.IO;
 using LiteDbSsRepositoryService;
 using ServiceInterfaces;
 
 namespace LogicTest
 {
-    public class LiteDbTests : StipiStopiTestBase
+    public class LiteDbTests : StipiStopiTestBase, IDisposable
     {
         override protected ISsRepository CreateRepository()
         {
-            // TODO Remove temp file on Dispose
-            return new LiteDbSsRepository(System.IO.Path.GetTempFileName()+".litedb");
+            var zeroFile = Path.GetTempFileName();
+            File.Delete(zeroFile);
+            tempLiteDbFile = zeroFile + ".litedb";
+            return new LiteDbSsRepository(tempLiteDbFile);
         }
+
+        public void Dispose()
+        {
+            if (tempLiteDbFile == null)
+                return;
+
+            File.Delete(tempLiteDbFile);
+            tempLiteDbFile = null;
+        }
+
+        private string tempLiteDbFile;
     }
 }
