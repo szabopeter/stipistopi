5dc68bff27c2926bae788224229fba4ed2ae9db0
Szabó Péter <1254135+szabopeter@users.noreply.github.com>
2020-05-31 07:53:20 +0000

    Wrap most js app logic into a new StipiStopi class

--- RestApi/wwwroot/main.js
+++ RestApi/wwwroot/main.js
@@ -2,7 +2,7 @@ import { AjaxLoad } from "./util.js";
 
 function DownloadTemplate(filename, loadAction) {
     // TODO: on failure
-    AjaxLoad(filename, "text", loadAction, function() {});
+    AjaxLoad(filename, "text", loadAction, function () { });
 }
 
 /**
@@ -16,9 +16,9 @@ function DownloadTemplate(filename, loadAction) {
 function LoadTemplates(templateFiles, loadAction) {
     let contents = {};
     let toLoadCount = templateFiles.length;
-    templateFiles.forEach(function(fn) {
+    templateFiles.forEach(function (fn) {
         let filename = fn;
-        DownloadTemplate(filename, function(content) {
+        DownloadTemplate(filename, function (content) {
             contents[filename] = content;
             console.log("stored " + filename)
             toLoadCount -= 1;
@@ -29,73 +29,105 @@ function LoadTemplates(templateFiles, loadAction) {
     });
 }
 
-function OnTemplatesLoaded(contents) {
-    console.log("Everything should be loaded by now...");
-    console.log(contents);
-    Handlebars.registerPartial("resourceLineInList", contents["resourceline.hbs"]);
-    let resourceListTemplate = Handlebars.compile(contents["resourcelist.hbs"]);
+function TemplateManager() {
+    let names = {
+        resourceLine: "resourceline.hbs",
+        resourceList: "resourcelist.hbs",
+    };
 
-    var resourceActions = { actions: [] };
+    var templateManager = {
+        // names: names,
+        templates: {},
+        resourceListTemplate: null,
+        Load: function (contents) {
+            Handlebars.registerPartial("resourceLineInList", contents[names.resourceLine]);
+            this.resourceListTemplate = Handlebars.compile(contents["resourcelist.hbs"]);
+            this.templates = contents;
+        },
+    };
+
+    return templateManager
+}
+
+function ResourceActions() {
+    var resourceActions = {
+        actions: []
+    };
     resourceActions.execute = function (index) { resourceActions.actions[index].execute(); };
     resourceActions.clear = function () { resourceActions.actions = []; };
-    resourceActions.create = function(label, fn) {
-        let action = {label: label, execute: fn, id: resourceActions.actions.length};
+    resourceActions.create = function (label, fn) {
+        let action = { label: label, execute: fn, id: resourceActions.actions.length };
         resourceActions.actions.push(action);
         return action;
     };
-    window.resourceActions = resourceActions;
-    console.log("window.resourceActions has been set");
+    return resourceActions;
+}
 
-    function SendResourceAction(action, resourceName) {
+function StipiStopi() {
+    let stipistopi = {
+        resourceActions: ResourceActions(),
+        templateManager: TemplateManager(),
+    }
+
+    stipistopi.SendResourceAction = function SendResourceAction(action, resourceName) {
         let lockParameter = {
             resourceName: resourceName,
-            user: {userName: "test", password: "test"},
+            user: { userName: "test", password: "test" },
         };
 
         let xhttp = new XMLHttpRequest();
         xhttp.responseType = "json";
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-                DownloadResourceList();
+                stipistopi.DownloadResourceList();
             }
+            // TODO : on failure?
         };
         xhttp.open("POST", "./stipistopi/" + action);
         xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
         xhttp.send(JSON.stringify(lockParameter));
-    }
+    };
 
-    let resourceList = document.getElementById("resourceList");
-    console.log("resourceList identified: ")
-    console.log(resourceList);
-
-    function UpdateResourceList(resources) {
+    stipistopi.UpdateResourceList = function UpdateResourceList(resources) {
+        let resourceActions = stipistopi.resourceActions;
         resourceActions.clear();
         resources.forEach(resource => {
             let label = resource.isAvailable ? "Lock" : "Release";
             let resourceAction = resource.isAvailable ? "lock" : "release";
-            let execute = function () { SendResourceAction(resourceAction, resource.shortName); };
+            let execute = function () { stipistopi.SendResourceAction(resourceAction, resource.shortName); };
             let action = resourceActions.create(
                 label,
                 execute
-                );
+            );
             resource.actions = [action];
         });
-        resourceList.innerHTML = resourceListTemplate(resources);
+        let resourceList = document.getElementById("resourceList");
+        resourceList.innerHTML = stipistopi.templateManager.resourceListTemplate(resources);
         console.log("Setting resourceList inner html...");
         // console.log(resourceList.innerHTML);
-        document.getElementById("buttonRefreshResourceList").addEventListener("click", DownloadResourceList);
-    }
+        document.getElementById("buttonRefreshResourceList").addEventListener("click", stipistopi.DownloadResourceList);
+    };
 
-    UpdateResourceList([]);
-
-    function DownloadResourceList() {
+    stipistopi.DownloadResourceList = function DownloadResourceList() {
         // TODO: on failure
-        AjaxLoad("./stipistopi/resources", "json", UpdateResourceList, function() {});
-    }
+        AjaxLoad("./stipistopi/resources", "json", stipistopi.UpdateResourceList, function () { });
+    };
+
+    stipistopi.OnTemplatesLoaded = function OnTemplatesLoaded(contents) {
+        console.log("Everything should be loaded by now...");
+        // console.log(contents);
+        stipistopi.templateManager.Load(contents);
+        stipistopi.DownloadResourceList();
+    };
+
+    return stipistopi;
 }
 
 function PageLoaded() {
-        LoadTemplates(["resourceline.hbs", "resourcelist.hbs"], OnTemplatesLoaded);
+    let stipistopi = StipiStopi();
+    window.resourceActions = stipistopi.resourceActions;
+    console.log("window.resourceActions has been set");
+    LoadTemplates(["resourceline.hbs", "resourcelist.hbs"], stipistopi.OnTemplatesLoaded);
 }
 
 // document.addEventListener("DOMContentLoaded", PageLoaded);
